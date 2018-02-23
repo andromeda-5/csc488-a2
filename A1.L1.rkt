07bd9e0ae540af03e690232baffaff03243df680
leirion <lily.aliyeva@mail.utoronto.ca>
2018-02-23 01:59:13 -0500

    added a1 files

---
+++ A1.L1.rkt
@@ -0,0 +1,183 @@
+#lang racket #| Compile Language L0 to Language L1 |#
+
+(provide debruijn index-λs L0→L1)
+(module+ test (require rackunit))
+(require racket/syntax)
+
+#| Language L0 is a slightly enriched version of the LC.
+
+ An L0 expression is defined structurally [recall CSC236 which covered reading such definitions].
+ It's one of: |#
+#;(L0: λ (<id>) <e>)
+#;(L0: app <e1> <e2>)
+#;(L0: var <id>)
+#;(L0: datum <i>)
+#;(L0: set! <id> <e>)
+;  where <e>, <e1>, <e2> are L0 expressions, <id> is an identifier, and <i> is an integer.
+
+; As shown, in this assignment we'll tag language terms with an initial symbol indicating which
+;  language they're from.
+
+; As usual, our runtime representation of such expressions is racket lists for parenthesized terms,
+;  and racket symbols for names.
+
+; This isn't the first few weeks of CSC108. You're months away from industry or graduate school,
+;  so you automatically and systematically generate examples of such expressions, to make the rest of
+;  this description concrete and meaningful. You also do this because you follow industry standard
+;  Test Driven Development.
+; We're still happy to help you develop these skills if you've managed to avoid that until now, but
+;  can't indulge requests that actively avoid learning those skills where they are required ---
+;  that's a waste of everyone's time in a course that is creating a modern compiler for a modern
+;  language down to a real architecture.
+
+#| Semantics of language L0 |#
+#;(L0: λ (<id>) <e>)  ; Unary closure creation.
+#;(L0: app <e1> <e2>) ; Unary function call.
+#;(L0: var <id>)      ; Variable access.
+#;(L0: datum <i>)     ; Integer constant.
+#;(L0: set! <id> <e>) ; Variable mutation: set <id> to the result of <e>.
+
+
+#| Language L1 |#
+
+; In the following, <n> is a natural number.
+#;(L1: λ <n> <e>)     ; The nth lambda in an L1 expression.
+#;(L1: app <e1> <e2>) ; Same meaning as in L0.
+#;(L1: var <n>)       ; Reference to a variable <n> scopes up.
+#;(L1: var <id>)      ; Free/unbound/open variable reference to <id>.
+#;(L1: set! <n> <e>)  ; Set the variable <n> scopes up to the value of <e>.
+#;(L1: datum <i>)     ; Same meaning as in L0.
+
+
+#| DeBruijn Indexing of an L0 Expression
+
+ Replaces each variable referencing a parameter, with a natural number indicating how many scopes up
+  the variable is. Free variables are left alone: in later passes they will turn into references to
+  pre-defined functions.
+
+ Leaves the form of other L0 expressions alone, but does rewrite their sub-expressions.
+
+ The optional argument env is a simple symbol table: it is the list of all ancestor parameters,
+  with their positions being an implicit mapping to how far up in scope they are. |#
+
+
+(module+ test
+  
+  (check-equal? (debruijn '(L0: datum 2)) '(L0: datum 2))
+  (check-equal? (debruijn '(L0: datum 2) '()) '(L0: datum 2))
+  (check-equal? (debruijn '(L0: datum 2) '(x y z)) '(L0: datum 2))
+  
+  (check-equal? (debruijn '(L0: var y)) '(L0: var y))
+  (check-equal? (debruijn '(L0: var y) (list)) '(L0: var y))
+  (check-equal? (debruijn '(L0: var y) '(x z)) '(L0: var y))
+  (check-equal? (debruijn '(L0: var y) '(x y z)) '(L0: var 1))
+
+  (check-equal? (debruijn '(L0: app (L0: var y) (L0: var x))) '(L0: app (L0: var y) (L0: var x)))
+  (check-equal? (debruijn '(L0: app (L0: var y) (L0: var x)) '(x y)) '(L0: app (L0: var 1) (L0: var 0)))
+
+  (check-equal? (debruijn '(L0: set! x (L0: datum 2)) '(y z x)) '(L0: set! 2 (L0: datum 2)))
+  (check-equal? (debruijn '(L0: set! x (L0: var y)) '(y x)) '(L0: set! 1 (L0: var 0)))
+  
+  (check-equal? (debruijn '(L0: λ (x) (L0: datum 2))) '(L0: λ (x) (L0: datum 2)))
+  (check-equal? (debruijn '(L0: λ (x) (L0: datum 2)) '(a b c x)) '(L0: λ (x) (L0: datum 2)))
+  (check-equal? (debruijn '(L0: λ (x) (L0: var x))) '(L0: λ (x) (L0: var 0)))
+  (check-equal? (debruijn '(L0: λ (x) (L0: λ (y) (L0: set! x (L0: var y))))) '(L0: λ (x) (L0: λ (y) (L0: set! 1 (L0: var 0))))))
+
+(define (debruijn e [env '()]) ; Takes an optional second argument, which defaults to the empty list.
+  (match e
+    [`(L0: var ,<id>) (cond [(index-of env <id>) `(L0: var ,(index-of env <id>))]
+                                         [else `(L0: var ,<id>)])]
+    [`(L0: app ,<e1> ,<e2>) `(L0: app ,(debruijn <e1> env) ,(debruijn <e2> env))]
+    [`(L0: set! ,<id> ,<e>) (cond [(index-of env <id>) `(L0: set! ,(index-of env <id>) ,(debruijn <e> env))]
+                                      [else `(L0: set! ,<id> ,(debruijn <e>))])]
+    [`(L0: λ (,<id>) ,<e>) `(L0: λ (,<id>) ,(debruijn <e> (append (list <id>) env)))]
+    [_  e]
+))
+
+
+#| Indexing λs of a Debruijnized L0 Expression
+
+ Replaces each L0 λ with an L1 λ, replacing the parameter list with a numeric index.
+ Indexing starts at the value produced by the optional counter argument count, and is applied
+  post-order when considering the expression as a tree. |#
+
+; A class to make counting objects.
+(define ((counter [c 0]))
+  (set! c (add1 c))
+  (sub1 c))
+
+(module+ test
+  (define c (counter))
+  (check-equal? (c) 0)
+  (check-equal? (c) 1)
+  (define c′ (counter))
+  (check-equal? (c′) 0)
+  (check-equal? (c) 2))
+
+
+(module+ test
+  (check-equal? (index-λs '(L0: var x)) '(L0: var x))
+  (check-equal? (index-λs '(L0: λ 2 (L0: datum 2))) '(L0: λ 2 (L0: datum 2)))
+  
+  (define d (counter))
+  (check-equal? (index-λs '(L0: λ (x) (L0: datum 2)) d) '(L0: λ 0 (L0: datum 2)))
+  
+  (define d1 (counter))
+  (check-equal? (index-λs '(L0: app (L0: λ (x) (L0: datum 2)) (L0: λ (x) (L0: datum 3))) d1)
+                '(L0: app (L0: λ 0 (L0: datum 2)) (L0: λ 1 (L0: datum 3))))
+  
+  (define d2 (counter))
+  (check-equal? (index-λs '(L0: λ (x) (L0: app (L0: λ (x) (L0: var 0)) (L0: λ (x) (L0: var 0)))) d2)
+                '(L0: λ 2 (L0: app (L0: λ 0 (L0: var 0)) (L0: λ 1 (L0: var 0)))))
+
+  (define d3 (counter))
+  (d3)
+  (d3)
+  (check-equal? (index-λs '(L0: λ (x) (L0: app (L0: λ (x) (L0: var 0)) (L0: λ (x) (L0: var 0)))) d3)
+                '(L0: λ 4 (L0: app (L0: λ 2 (L0: var 0)) (L0: λ 3 (L0: var 0))))
+  ))
+
+; For a debruijned L0 expression, give each λ expression in e a unique index.
+(define (index-λs e [count (counter)])
+  (match e
+    [`(L0: λ (,<n>) ,<e>) (let ([temp (index-λs <e> count)]) `(L0: λ ,(count) ,temp))]
+    [`(L0: app ,<e1> ,<e2>) `(L0: app ,(index-λs <e1> count) ,(index-λs <e2> count))]
+    [`(L0: set! ,<n> ,<e>) `(L0: set! ,<n> ,(index-λs <e> count))]
+    [_ e]))
+
+
+#| L0→L1
+
+ For an L0 expression: debruijnizes, indexes λs, and replaces remaining ‘L0:’ tags with ‘L1:’. |#
+
+(module+ test
+  (check-equal? (L0→L1 '(L0: datum 488)) '(L1: datum 488))
+  (check-equal? (L0→L1 '(L0: var x)) '(L1: var x))
+  (check-equal? (L0→L1 '(L0: λ (x) (L0: var x))) '(L1: λ 0 (L1: var 0)))
+  (check-equal? (L0→L1 '(L0: set! x (L0: datum 488)))
+                '(L1: set! x (L1: datum 488)))
+  (check-equal? (L0→L1 '(L0: λ (x) (L0: set! x (L0: datum 488))))
+                '(L1: λ 0 (L1: set! 0 (L1: datum 488))))
+  (check-equal? (L0→L1 '(L0: app (L0: var x) (L0: var y)))
+                '(L1: app (L1: var x) (L1: var y)))
+  (check-equal? (L0→L1 '(L0: λ (x) (L0: set! x (L0: datum 488))))
+                '(L1: λ 0 (L1: set! 0 (L1: datum 488))))
+  (check-equal? (L0→L1 '(L0: λ (x) (L0: app (L0: λ (y) (L0: var y)) (L0: λ (z) (L0: datum 488)))))
+                '(L1: λ 2 (L1: app (L1: λ 0 (L1: var 0)) (L1: λ 1 (L1: datum 488))))) 
+  (check-equal? (L0→L1 '(L0: app (L0: app (L0: var +) (L0: datum 3)) (L0: datum 4)))
+                '(L1: app (L1: app (L1: var +) (L1: datum 3)) (L1: datum 4)))
+  (check-equal? (L0→L1 '(L0: app (L0: app (L0: var *) (L0: datum 3)) (L0: datum 4)))
+                '(L1: app (L1: app (L1: var *) (L1: datum 3)) (L1: datum 4)))
+  (check-equal? (L0→L1 '(L0: app (L0: λ (x) (L0: app (L0: app (L0: var +) (L0: datum 3)) (L0: var x))) (L0: datum 5)))
+                '(L1: app (L1: λ 0 (L1: app (L1: app (L1: var +) (L1: datum 3)) (L1: var 0))) (L1: datum 5)))
+  )
+
+(define (L0→L1 e)
+  (define (L0→L1′ e)
+    (match e
+      [`(L0: var ,<n>) `(L1: var ,<n>)]
+      [`(L0: app ,<e1> ,<e2>) `(L1: app ,(L0→L1′ <e1>) ,(L0→L1′ <e2>))]
+      [`(L0: datum ,<i>) `(L1: datum ,<i>)]
+      [`(L0: set! ,<n> ,<e>) `(L1: set! ,<n> ,(L0→L1′ <e>))]
+      [`(L0: λ ,<n> ,<e>) `(L1: λ ,<n> ,(L0→L1′ <e>))]))
+  (L0→L1′ (index-λs (debruijn e))))
---
+++ A1.L2.rkt
@@ -0,0 +1,162 @@
+#lang racket #| Compile L1 to s-expression-machine language L2 |#
+
+(provide (struct-out compiled:L2) L1→L2)
+(module+ test (require rackunit))
+
+#| Language L2
+
+ This is a machine-like language, for a machine that supports enough to track the extended information
+  in the memory model. Along with the support to model the LC, it has support for variable mutation,
+  integer constants, and a few named lambdas.
+
+ There are six types of statement in L2.
+ All expression results move through a special result location. |#
+
+#;(L2: closure <name>)
+; Create a unary closure from the compiled body named <name>.
+; Leave a reference to it in result.
+
+#;(L2: variable <n>)
+; Look up the variable <n> environments up from the current environment, put its value in result.
+
+; Put the value of result on the stack.
+#;(L2: push_result)
+
+; Call the closure that is on the stack, with the argument that is in result.
+#;(L2: call)
+
+; Set the variable <n> environments up from the current environment, to the value of result.
+#;(L2: set <n>)
+
+; Put integer <i> in result.
+#;(L2: set_result <i>)
+
+
+#| Compiling L1 to L2
+
+ From your tracing examples, you understand how to track the memory model via the above operations.
+
+ Compiling an expression in L1 produces:
+  • code: a list of L2 statements for the expression
+  • λs: a list of two-element lists (<name> <code>) with the <name>s of the λs in the expression
+     paired with the code for their bodies
+
+ The following summarizes your understanding.
+ These *descriptions* are slightly informal, in particular they blur the distinction between
+  lists and individual elements of a list. Don't blindly turn them into an implementation! |#
+
+#;{(L1: λ <n> <e>) →
+                   code: (L2: closure lambda_<n>)
+                   λs: {(lambda_<n> <code-for-e>)
+                        <λs-for-e>}}
+
+#;{(L1: app <e1> <e2>) →
+                       code: {<code-for-e1>
+                              (L2: push_result)
+                              <code-for-e2>
+                              (L2: call)}
+                       λs: {<λs-for-e1>
+                            <λs-for-e2>}}
+
+#;{(L1: set! <n> <e>) →
+                      code: {<code-for-e>
+                             (L2: set <n>)}
+                      λs: {<λs-for-e>}}
+
+; Each of the following produce a single L2 statement and no λs:
+#;{(L1: var <n>) → (L2: variable <n>)}
+#;{(L1: var +) → (L2: closure make_add)}
+#;{(L1: var *) → (L2: closure make_multiply)}
+#;{(L1: var <) → (L2: closure make_less_than)}
+#;{(L1: datum <i>) → (L2: set_result <i>)}
+
+
+#| L1→L2
+
+ Each expression produces an instance of struct compiled:L2, which has two fields for code and λs. |#
+
+(struct compiled:L2 (code λs) #:transparent)
+
+(module+ test
+  ; Along with making a constructor named ‘compiled:L2’ and also a pattern with that name,
+  ;  the struct declaration implicitly defines two field accessor functions: ‘compiled:L2-code’
+  ;  and ‘compiled:L2-λs’, which you might prefer sometimes instead of needing to name results
+  ;  via pattern matching.
+  (define c (compiled:L2 '((L2: set_result 123)) '((lambda_0 ((L2: variable 0))))))
+  (check-equal? (match c [(compiled:L2 some-code some-λs) some-code])
+                (compiled:L2-code c))
+  (check-equal? (match c [(compiled:L2 some-code some-λs) some-λs])
+                (compiled:L2-λs c)))
+
+; Produce a symbol of the form lambda_<n>.
+(define (lambda_ n)
+  (local-require (only-in racket/syntax format-symbol))
+  (format-symbol "lambda_~a" n))
+
+(module+ test
+  (check-equal? (lambda_ 123) 'lambda_123))
+
+(module+ test
+  (check-equal? (L1→L2 '(L1: var 0)) (compiled:L2 '((L2: variable 0)) '()))
+  (check-equal? (L1→L2 '(L1: var 4)) (compiled:L2 '((L2: variable 4)) '()))
+  (check-equal? (L1→L2 '(L1: var +)) (compiled:L2 '((L2: closure make_add)) '()))
+  (check-equal? (L1→L2 '(L1: var *)) (compiled:L2 '((L2: closure make_multiply)) '()))
+  (check-equal? (L1→L2 '(L1: var <)) (compiled:L2 '((L2: closure make_less_than)) '()))
+  (check-equal? (L1→L2 '(L1: datum 2))
+                (compiled:L2 '((L2: set_result 2))
+                             '()))
+  (check-equal? (L1→L2 '(L1: set! 0 (L1: datum 2)))
+                (compiled:L2 '((L2: set_result 2) (L2: set 0))
+                             '()))
+  (check-equal? (L1→L2 '(L1: app (L1: datum 2) (L1: datum 3)))
+                (compiled:L2 '((L2: set_result 2) (L2: push_result) (L2: set_result 3) (L2: call))
+                             '()))
+  (check-equal? (L1→L2 '(L1: app (L1: λ 0 (L1: set! 0 (L1: datum 488))) (L1: var 2)))
+                (compiled:L2 '((L2: closure lambda_0) (L2: push_result) (L2: variable 2) (L2: call))
+                             '((lambda_0 ((L2: set_result 488) (L2: set 0))))))
+  (check-equal? (L1→L2 '(L1: λ 0 (L1: datum 2)))
+                (compiled:L2 '((L2: closure lambda_0))
+                             '((lambda_0 ((L2: set_result 2))))))
+  (check-equal? (L1→L2 '(L1: λ 1 (L1: λ 0 (L1: datum 2))))
+                (compiled:L2 '((L2: closure lambda_1))
+                             '((lambda_1 ((L2: closure lambda_0)))
+                               (lambda_0 ((L2: set_result 2))))))
+  (check-equal? (L1→L2 '(L1: λ 0 (L1: app (L1: datum 2) (L1: datum 3))))
+                (compiled:L2 '((L2: closure lambda_0))
+                             '((lambda_0 ((L2: set_result 2) (L2: push_result) (L2: set_result 3) (L2: call))))
+                             ))
+  (check-equal? (L1→L2 '(L1: app (L1: app (L1: var +) (L1: datum 3)) (L1: datum 4)))
+                        (compiled:L2 '((L2: closure make_add) (L2: push_result) (L2: set_result 3) (L2: call) (L2: push_result) (L2: set_result 4) (L2: call))
+                                     '()))
+  (check-equal? (L1→L2 '(L1: app (L1: app (L1: var *) (L1: datum 3)) (L1: datum 4)))
+                        (compiled:L2 '((L2: closure make_multiply) (L2: push_result) (L2: set_result 3) (L2: call) (L2: push_result) (L2: set_result 4) (L2: call))
+                                     '()))
+  (check-equal? (L1→L2 '(L1: app (L1: λ 0 (L1: app (L1: app (L1: var +) (L1: datum 3)) (L1: var 0))) (L1: datum 5)))
+                        (compiled:L2 '((L2: closure lambda_0) (L2: push_result) (L2: set_result 5) (L2: call))
+                                     '((lambda_0 ((L2: closure make_add) (L2: push_result) (L2: set_result 3) (L2: call) (L2: push_result) (L2: variable 0) (L2: call)))))
+                )
+  )
+
+(define (L1→L2 e)
+  (match e
+    [`(L1: var +) (compiled:L2 '((L2: closure make_add)) '())]
+    [`(L1: var *) (compiled:L2 '((L2: closure make_multiply)) '())]
+    [`(L1: var <) (compiled:L2 '((L2: closure make_less_than)) '())]
+    [`(L1: var ,<n>) (compiled:L2 `((L2: variable ,<n>)) '())]
+    [`(L1: datum ,<i>) (compiled:L2 `((L2: set_result ,<i>)) '())]
+    [`(L1: set! ,<n> ,<e>) (let ([e (L1→L2 <e>)])
+                             (compiled:L2
+                              (append (compiled:L2-code e) `((L2: set ,<n>)))
+                              (compiled:L2-λs e)))]
+    [`(L1: app ,<e1> ,<e2>) (let ([e1 (L1→L2 <e1>)] [e2 (L1→L2 <e2>)])
+                              (compiled:L2
+                               (append (compiled:L2-code e1) (list '(L2: push_result)) (compiled:L2-code e2) (list '(L2: call)))
+                               (append (compiled:L2-λs e1) (compiled:L2-λs e2))))]
+    [`(L1: λ ,<n> ,<e>) (let ([e (L1→L2 <e>)])
+                             (compiled:L2
+                              `((L2: closure ,(lambda_ <n>)))
+                              (cons (list (lambda_ <n>) (compiled:L2-code e)) (compiled:L2-λs e))))]
+
+                             
+    )
+  )
